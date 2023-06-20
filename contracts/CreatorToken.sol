4eff229733a2972ad75f40fc30c2fca606222088
yuk6ra <yuki.imamura.all@gmail.com>
2023-06-20 11:56:18 +0900

    Add struct Creator

--- contracts/CreatorToken.sol
+++ contracts/CreatorToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 import "./interfaces/IERC6551Registry.sol";
 
 contract CreatorToken is ERC721, Ownable, ReentrancyGuard {
@@ -14,9 +15,33 @@ contract CreatorToken is ERC721, Ownable, ReentrancyGuard {
 
     constructor() ERC721("CreatorToken", "CT") {}
 
-    function mint() public {
+    struct Creator {
+        address creatorAddress;
+        string name;
+        string description;
+        string website;
+        string profilePicture;
+    }
+
+    mapping (uint256 => Creator) public creators;
+
+    function mint(
+        address _creatorAddress,
+        string memory _name,
+        string memory _description,
+        string memory _website,
+        string memory _profilePicture
+    ) public {
         _mint(msg.sender, totalSupply);
         createTokenBoundAccount(totalSupply);
+
+        creators[totalSupply] = Creator(
+            _creatorAddress,
+            _name,
+            _description,
+            _website,
+            _profilePicture
+        );
 
         totalSupply++;
     }
@@ -51,5 +76,30 @@ contract CreatorToken is ERC721, Ownable, ReentrancyGuard {
     ) external onlyOwner {
         factoryContract = _factoryContract;
         implementationContract = _implementationContract;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {        
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        string memory image = creators[tokenId].profilePicture;
+
+        return string(
+            abi.encodePacked(
+                "data:application/json;base64,",
+                Base64.encode(
+                    bytes(
+                        abi.encodePacked(
+                            '{"name":"',
+                            creators[tokenId].name,
+                            '", "description":"',
+                            creators[tokenId].description,
+                            '", "external_url":"',
+                            creators[tokenId].website,
+                            '", "image":"',
+                            image,
+                            '"}'
+                        )
+                    )
+                ))
+        );
     }
 }
